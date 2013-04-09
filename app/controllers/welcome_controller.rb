43f8e7d7bde7e88ed61548f887e0eed522400c50
Jonas Oggier <jonas.oggier@gmail.com>
2013-04-09 19:37:51 -0400

    Boostrapping design of index page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+
+	#Homepage
+	def index		
+	end
+	
+end
--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -14,7 +14,27 @@ class ProjectFlowsTest < ActionDispatch::IntegrationTest
 		assert page.has_content?('Wifi-enabled shoes')
 		assert page.has_content?('Waterproof Whiteboard')
 		assert page.has_content?('Something else')
+
+		# Go to the project details (show) page
+		click_link "Waterproof Whiteboard"
+		assert_equal project_path(project2), current_path
+		assert find("h1:first").has_content? project2.title
   end
 
+  test "navigation" do
+  	visit '/'
+		assert_equal root_path, current_path
+		assert_equal "Home", find("ul.nav li.active a").text
+
+		visit '/projects'
+		assert_equal projects_path, current_path
+		assert_equal "Projects", find("ul.nav li.active a").text
+  end
+
+  test "show page" do
+  	project1 = FactoryGirl.create(:project, id: 1)
+   	visit '/projects/1'
+		assert page.has_content?('Wifi-enabled shoes')
+  end
 
 end
