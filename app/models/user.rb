829c2908c70c7b1d767cb4b948738d92266f2132
Jonas Oggier <jonas.oggier@gmail.com>
2013-04-10 09:14:25 -0400

    Sign-up functionality added + corresponding tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   
-
   has_many :projects
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true, :on => :create
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Crowdfunder::Application.routes.draw do
 	root :to => 'welcome#index'  
   
   resources :projects
+  resources :users, :except => [:index, :destroy]
 
   
 
